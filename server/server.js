2dc4655527401003e60a532296fea599837a6b12
Benoit <benoit.caccinolo@gmail.com>
2017-06-26 16:43:18 +0200

    Todo server side is working

--- server/server.js
+++ server/server.js
@@ -1,18 +1,39 @@
 
 var express = require('express');
-var morgan = require('morgan');
+var morgan = require('morgan'); // requests log
 
 var app = express();
-
 app.use(morgan('dev'));
 
+app.todos = ["faire les courses", "aller faire la vidange"];
+
 app.get('/', function(req, res){
-  res.send('coucou le monde');
+
+  res.json(app.todos);
+
 });
 
-app.get('/users', function(req, res){
-  res.json({data:'la liste des utilisateurs'});
+app.get('/set', function(req, res){
+
+  var new_todo = req.query.todo
+
+  if (new_todo != '') {
+    app.todos.push(new_todo);
+  }
+
+  res.json(app.todos);
 });
+
+app.get('/delete/:id', function(req, res){
+
+  if (req.params.id != '') {
+    app.todos.splice(req.params.id, 1);
+  }
+
+  res.json(app.todos);
+
+});
+
 
 app.listen(3001);
 
