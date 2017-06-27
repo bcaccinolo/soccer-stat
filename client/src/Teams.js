ed178fefb3f86fe3f498fe873b091d2dde78f231
Benoit <benoit.caccinolo@gmail.com>
2017-06-27 18:12:26 +0200

    graph area updates correctly

--- client/src/Teams.js
+++ client/src/Teams.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 class TeamsTable extends Component {
 
@@ -8,7 +9,18 @@ class TeamsTable extends Component {
 
   componentDidMount() {
     fetch('/teams').then(res => res.json())
-                   .then(data => this.setState({teams: data}));
+                   .then(data => this.setState({teams: data}))
+                   .then(state => this.displayGraph(1));
+
+  }
+
+  displayGraph(i){
+    console.log('display graph');
+    console.log(i);
+    console.log(this.state.teams[i]);
+
+    const team = this.state.teams[i];
+    ReactDOM.render(<TeamGraph data={team} />, document.getElementById('teamgraph'));
 
   }
 
@@ -30,7 +42,9 @@ class TeamsTable extends Component {
              </tr>
             </thead>
             <tbody>
-              { teams.map((team, index) => <TeamEntry key={team.rank} data={team} /> ) }
+              { teams.map((team, index) => <TeamEntry key={team.rank}
+                                                      onClick={() => this.displayGraph(index) }
+                                                      data={team} /> ) }
             </tbody>
           </table> )
 
@@ -52,21 +66,53 @@ class TeamEntry extends Component {
     this.state = props.data;
   }
 
+  handleClick(e){
+    console.log('click on a team entry');
+  }
+
   render () {
 
     const {rank, logo, team, points} = this.state;
     const logo_path = "images/logos/" + logo + ".gif";
 
     return(
-      <tr key={rank} >
+      <tr key={rank} onClick={this.props.onClick} >
         <td>{rank}</td>
-        <td><img src={logo_path} /></td>
+        <td><img src={logo_path} alt='' /></td>
         <td>{team}</td>
         <td>{points}</td>
       </tr>
     )
   }
+}
 
+class TeamGraph extends Component {
+
+  constructor(props){
+    super();
+    this.state = props.data;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(nextProps.data);
+  }
+
+  render(){
+    const {rank, logo, team, points} = this.state;
+    const logo_path = "images/logos/" + logo + ".gif";
+
+    return( <div className="panel panel-default">
+              <div className="panel-heading">
+                  <h3 className="panel-title">
+                    <img src={logo_path} alt='' /> {this.state.team}
+                  </h3>
+                </div>
+              <div className="panel-body">
+                Basic panel example
+              </div>
+            </div>
+    )
+  }
 }
 
 export default TeamsTable;
