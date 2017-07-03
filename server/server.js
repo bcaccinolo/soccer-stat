70866e4a4ba10d018f2ca26fab8b588a7d3df2f1
Benoit <benoit.caccinolo@gmail.com>
2017-07-03 13:24:33 +0200

    [server] addition of GET /team/:championship
    This call allow to get different championship ranking.

--- server/server.js
+++ server/server.js
@@ -15,46 +15,28 @@ app.use(bodyParser.json());
 
 app.get('/teams', function(req, res){
 
-  var team_file = './data/table.json'
+  var team_file = './data/ligue1.json'
   jsonfile.readFile(team_file, function(err, obj) {
     res.json(obj);
   })
 
-
 });
 
-app.get('/set', function(req, res){
+app.get('/team/:name', function(req, res){
 
-  var new_todo = req.query.todo
+  var team_file = './data/' + req.params.name + '.json'
 
-  if (new_todo != '') {
-    app.todos.push(new_todo);
-  }
-
-  res.json(app.todos);
-});
-
-app.get('/todos/delete/:id', function(req, res){
-
-  if (req.params.id != '') {
-    app.todos.splice(req.params.id, 1);
-  }
-
-  res.json(app.todos);
+  jsonfile.readFile(team_file, function(err, obj) {
+    if (err != null) {
+      console.log(err);
+      res.status(404).send({error: 'error team not found'});
+    } else {
+      res.json(obj);
+      res.status(200).end();
+    }
+  })
 
 });
-
-app.post('/todos/new', function(req, res){
-
-  var new_todo = req.body.todo
-
-  if (new_todo != '') {
-    app.todos.push(new_todo);
-  }
-
-  res.json(app.todos);
-});
-
 
 app.listen(3001);
 
